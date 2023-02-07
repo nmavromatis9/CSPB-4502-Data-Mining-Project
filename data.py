6c9f0f6d0953f2c9edcff9e29c38d3b73bc1d5e1
nmavromatis9 <52474885+nmavromatis9@users.noreply.github.com>
2023-02-07 12:04:35 -0500

    created integrated csv

--- data.py
+++ data.py
@@ -4,11 +4,18 @@ import dask.dataframe as dd
 def main(): 
     print("hello World!")
     try:
-        df = pd.read_csv('D:/DesktopCopy/dataProject/bike/Accidents.csv')
-        print(df)
+        df = pd.read_csv('C:/Users/shogu/Python_files/Data/Project/bikeData/Accidents.csv')
+        df2 = pd.read_csv('C:/Users/shogu/Python_files/Data/Project/bikeData/Bikers.csv')
+        #print(df)
     except:
         print("An exception occurred!!!")
     
+    df3=pd.merge(df, df2, on="Accident_Index")
+    #print(df3)
+    try:
+        df3.to_csv(r'C:/Users/shogu/Python_files/Data/Project/bikeData/bikeData.csv', index=False)
+    except:
+        print("CSV output error")
     return 0
 
 if __name__ == "__main__":
